93a18f1a9289255c889e88f059d514dca6bdf809
qzlydao <qzlydao@126.com>
2019-02-01 16:17:22 +0800

    整合redis

--- src/main/java/com/qzlydao/flas/bizz/web/controller/DemoController.java
+++ src/main/java/com/qzlydao/flas/bizz/web/controller/DemoController.java
@@ -1,12 +1,17 @@
 package com.qzlydao.flas.bizz.web.controller;
 
+import com.qzlydao.flas.bizz.dao.MatchDao;
+import com.qzlydao.flas.commom.redis.RedisService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,6 +30,29 @@ public class DemoController {
         map.put("aa", "11");
         map.put("bb", "22");
         return map;
+    }
+
+    @Autowired private MatchDao matchDao;
+
+    @GetMapping("/getCount")
+    public Map<String, Object> getCount() {
+        int i = matchDao.queryCount();
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("count", i);
+        return resp;
+    }
+
+    @Autowired private RedisService redisService;
+
+    @PostMapping("/redisDemo")
+    public Object redisDemo(@RequestBody Map<String, Object> reqParam) {
+        Set<Map.Entry<String, Object>> entries = reqParam.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            redisService.set(key, value);
+        }
+        return null;
     }
 
 }
